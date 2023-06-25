2322d16aeb45bf4adfb0b783e9606931f0d5772b
Casey A. Childers <cachilders@gmail.com>
2023-06-25 06:03:59 +0100

    configs: add enzo and mtet

---
+++ lib/devices/meris_enzo.lua
@@ -0,0 +1,43 @@
+return {
+  make = "Meris",
+  model = "Enzo",
+  control = {
+    [4] = "Expression Pedal",
+    [9] = "Envelope Type",
+    [14] = "Bypass",
+    [15] = "Tempo",
+    [16] = "Pitch",
+    [17] = "Filter",
+    [18] = "Mix",
+    [19] = "Sustain",
+    [20] = "Filter Envelope",
+    [21] = "Modulation",
+    [22] = "Portamento",
+    [23] = "Filter Type",
+    [24] = "Delay Level",
+    [25] = "Ring Modulation",
+    [26] = "Filter Bandwidth",
+    [27] = "Delay Feedback",
+    [28] = "Tap",
+    [29] = "Synth Mode",
+    [30] = "Synth Waveshape"
+  },
+  program = {
+    [1] = "PolySwell",
+    [2] = "PolyRing",
+    [3] = "SparkArp",
+    [4] = "MonoEcho",
+    [5] = "PolyGlide",
+    [6] = "DarkArp",
+    [7] = "Mono Tyrell",
+    [8] = "ShiftTrem",
+    [9] = "BellLab",
+    [10] = "Woodwind",
+    [11] = "RileyCurve",
+    [12] = "PondBase",
+    [13] = "StarLand",
+    [14] = "FormantRing",
+    [15] = "LinkArp",
+    [16] = "ExpSweep"
+  }
+}
---
+++ lib/devices/obne_mtet.lua
@@ -0,0 +1,12 @@
+return {
+  make = "OBNE",
+  model = "MTET",
+  control = {
+    [1] = "Expression 1",
+    [2] = "Expression 2",
+    [3] = "Expression 3",
+    [4] = "Expression 4"
+  },
+  program = {
+  }
+}
